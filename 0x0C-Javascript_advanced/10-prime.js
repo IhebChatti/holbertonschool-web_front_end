3a8336ff9f00c9a2453660e8e5dbde4a706670a5
IhebChatti <iheb.chatti@holbertonschool.com>
2021-02-15 17:21:35 +0100

    exec time and primes

--- 0x0C-Javascript_advanced/10-prime.js
+++ 0x0C-Javascript_advanced/10-prime.js
@@ -1,5 +1,21 @@
 function countPrimeNumbers() {
-  return 25;
+  let primes = 0;
+  let isprime = true;
+  for (let counter = 2; counter <= 100; counter = counter + 1) {
+    isprime = true;
+    let limit = Math.round(Math.sqrt(counter));
+    for (let mod = 2; mod <= limit; mod++) {
+      if (counter % mod == 0) {
+        isprime = false;
+        break;
+      }
+    }
+
+    if (isprime) {
+      primes = primes + 1;
+    }
+  }
+  return primes;
 }
 
 let start = performance.now();
