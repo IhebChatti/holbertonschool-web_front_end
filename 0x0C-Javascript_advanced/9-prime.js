6dc8f90e695e39e78c917be8abd3bee811d9f0d8
IhebChatti <iheb.chatti@holbertonschool.com>
2021-02-15 17:14:08 +0100

    execution time

---
+++ 0x0C-Javascript_advanced/9-prime.js
@@ -0,0 +1,12 @@
+function countPrimeNumbers() {
+  return 25;
+}
+
+let start = performance.now();
+countPrimeNumbers();
+let end = performance.now();
+console.log(
+  "Execution time of printing countPrimeNumbers was " +
+    (end - start) +
+    " milliseconds."
+);
