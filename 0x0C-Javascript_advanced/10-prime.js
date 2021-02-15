0d53102532251c2898e1ebf0b71a5f22d4fa43a7
IhebChatti <iheb.chatti@holbertonschool.com>
2021-02-15 17:19:07 +0100

    execution time again

---
+++ 0x0C-Javascript_advanced/10-prime.js
@@ -0,0 +1,14 @@
+function countPrimeNumbers() {
+  return 25;
+}
+
+let start = performance.now();
+for (let i = 1; i <= 100; i++) {
+  countPrimeNumbers();
+}
+let end = performance.now();
+console.log(
+  "Execution time of calculating prime numbers 100 times was " +
+    (end - start) +
+    " milliseconds."
+);
